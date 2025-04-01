1a8aaacbb193d38a3361350373eead9b6d4b8785
ACCION1010 <accionfinancierap@fp.skandia.com.co>
2025-04-01 11:47:02 -0500

    Create _app.js

---
+++ pages/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+Agregar archivo _app.js para aplicar estilos globales
